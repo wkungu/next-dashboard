84ea004ba2d5df682e5dc66cf66afc7e41395de7
William N Kungu <wkungu@gmail.com>
2023-11-10 23:40:37 +0300

    fixed matcher and next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "",
+    output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'next-dashboard-rho-nine.vercel.app',
+                pathname: '**',
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
